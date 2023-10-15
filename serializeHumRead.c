d3b08ace2c1d9784994001d38e8ee5b8bab6f9bf
Daniel Schulz <daniel.schulz.lg@gmail.com>
2023-10-15 13:14:50 +0200

    Serialize data from Person p1 object to file people.dat

--- serializeHumRead.c
+++ serializeHumRead.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
+const char* PERSON_FORMAT_OUT = "(%s, %d, %c)\n";
+
 typedef struct Person {
     char    name[20];
     int     age;
     char    gender;
 } Person;
-
 
 int main(void) {
 
@@ -15,6 +16,20 @@ int main(void) {
         .gender = 'M',
     };
     printf("Name: %s, Age: %d, Gender: %c\n", p1.name, p1.age, p1.gender);
+
+    // Open file
+    FILE *f;
+    f = fopen("people.dat", "w+");
+    if (f == NULL) {
+        return 1;
+    }
+
+    // Serialization - Write data to file from memory
+    fprintf(f, PERSON_FORMAT_OUT, p1.name, p1.age, p1.gender);
+    fseek(f, 0, SEEK_SET);
+
+    // Close file
+    fclose(f);
 
     return 0;
 }
